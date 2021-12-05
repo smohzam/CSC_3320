592529b94a8f68aeabb1689b1c743080930df0be
srimohtheg <95510686+srimohtheg@users.noreply.github.com>
2021-12-04 19:06:18 -0500

    Add files via upload

---
+++ Lab6/hello.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo ' '
+echo Welcome to computer science society.
+
+#Sriram Mohan, smohan6@student.gsu.edu
+
+echo 
+date
+
+echo -n 'No.of directories in home: '  
+ls ~ | wc -l
+
+echo  $PATH
+echo  $USER
+echo  $SHELL
+echo ' '
+df
+echo ' '
+
+echo 'Please, could you loan me $25.00?' 
+echo 'if x = 2,x * x = 4, x/2 = 1'
+
+ls c*.sh
+
+echo -n 'Goodbye, current hour: '
+date +%I
+echo ' '
